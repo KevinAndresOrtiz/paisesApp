034988ed55228e05e7a18871551bcb3fcebf15d5
kevin Andres Ortiz Merchan <kevinandresortizmerchan@outlook.com>
2022-08-10 14:53:48 -0500

    add routes and refactoring component paises

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
-
-
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -10,6 +9,7 @@ import { SidebarComponent } from './sidebar/sidebar.component';
   ],
   exports:[SidebarComponent],
   imports: [
+    RouterModule,
     CommonModule
   ]
 })

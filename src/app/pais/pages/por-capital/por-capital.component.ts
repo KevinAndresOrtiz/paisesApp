7c432ffc84135c6a625931212dace7fbb3db31b9
Kevin Ortiz Merchan <kevin.ortiz@globant.com>
2022-08-11 13:50:30 -0500

    added component capital

--- src/app/pais/pages/por-capital/por-capital.component.ts
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-capital',
   templateUrl: './por-capital.component.html',
   styleUrls: ['./por-capital.component.css']
 })
-export class PorCapitalComponent implements OnInit {
+export class PorCapitalComponent {
+  termino:string = '';
+  existError: boolean = false;
+  showTable: boolean = false
+  private _paises: Country[] = [];
 
-  constructor() { }
+  constructor(private paisService: PaisService) {}
 
-  ngOnInit(): void {
+  get paises(){
+    return [...this._paises];
+  }
+
+  buscar(termino: string){
+    this.paisService.buscarCapital(termino)
+                    .subscribe({
+                      next: (data) => {
+                        this.existError = false;
+                        this._paises = data;
+                        this.showTable = true
+                      },
+                      error: (__) => {
+                        this.existError = true;
+                        this._paises = []
+                        this.showTable = false;
+                      }
+                    });
   }
 
 }

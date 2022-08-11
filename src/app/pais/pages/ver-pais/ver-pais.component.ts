6b127f642666fb03421caae1a1cde1904a1303dc
Kevin Ortiz Merchan <kevin.ortiz@globant.com>
2022-08-11 14:12:20 -0500

    subscribe ver pais

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-ver-pais',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerPaisComponent implements OnInit {
 
-  constructor() { }
+  constructor( private activateRoute: ActivatedRoute,
+               private paisService: PaisService) { }
 
   ngOnInit(): void {
+    this.activateRoute
+                      .params
+                      .subscribe(({id}) => {
+                          this.paisService.getPaisPorCodigo(id).subscribe(pais => {
+                              console.log(pais);
+                          });
+                      })
   }
 
 }
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -20,4 +20,10 @@ export class PaisService {
     const url = `${this.apiUrl}/capital/${termino}`;
     return this.http.get<Country[]>(url);
   }
+
+  getPaisPorCodigo( id: string ):Observable<Country> {
+    const url = `${this.apiUrl}/alpha/${id}`;
+    return this.http.get<Country>(url);
+
+  }
 }

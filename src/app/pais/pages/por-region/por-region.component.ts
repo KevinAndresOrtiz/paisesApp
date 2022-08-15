504a34a4aa5e53e8390d8f8768fd03655a95daa0
Kevin Ortiz Merchan <kevin.ortiz@globant.com>
2022-08-15 15:00:33 -0500

    filter by Region

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PorRegionComponent implements OnInit {
 
-  constructor() { }
+  regiones: string[] = ['EU','EFTA','CARICOM','PA','AU','USAN','EEU','AL','ASEAN','CAIS','CAFTA','NAFTA','SAARC']
+  regionActiva: string = '';
+  private _paisesRegion: Country[] = [];
+  constructor(private paisService: PaisService ) { }
 
   ngOnInit(): void {
+  }
+
+  get paisesRegion(){
+    return this._paisesRegion
+  }
+
+  activarRegion(region: string) {
+    if(region == this.regionActiva) {return}
+    this.regionActiva = region;
+              this.paisService
+                             .buscarRegion(region)
+                             .subscribe({
+                              next: (data) => {
+                                 this._paisesRegion = data
+                              },
+                              error: (__) => {
+                                this._paisesRegion = []
+                              }
+                             })
+  }
+
+  getClassCSS(region: string){
+    return ( region === this.regionActiva) ? 'btn btn-primary' : 'btn btn-outline-primary'
   }
 
 }
